3d03027217e0fcf69e95ad9db40f73de293cff41
byronajcet17 <byronajcet@gmail.com>
2026-03-09 09:33:44 -0600

    #publish

---
+++ Inventario.py
@@ -0,0 +1,19 @@
+def agregar_productos(inventario):
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio: "))
+    cantidad = int(input("Cantidad: "))
+    
+    inventario.append({
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad,
+    })
+
+def eliminar_productos():
+ print("Producto a eliminar")
+ for Inventario in inventario.append:
+     inventarion.append
+     
+     
+def ver_productos():
+    
--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
+import Inventario
+from Inventario import agregar_productos
 menu = """
  ===========================================    
          Administrador de Colección
  ===========================================
 1. Añadir un Producto
 2. Eliminar productos
-3. Ver producots
+3. Ver productos
 4. Salir
  ===========================================
  """   
@@ -12,10 +14,11 @@ while True:
  print(menu)
  opc = input ("Selecciona una opción: ")
  if opc == "1":
-    if opc == "2":
-     if opc == "3":
-      if opc == "4":
-       print("Feliz día")
+     agregar_productos()
+     if opc == "2":
+      if opc == "3":
+       if opc == "4":
+        print("Feliz día")
  break
 
 
