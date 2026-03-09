c19df1023c555ec3378e542ad72713f7604d88b2
byronajcet17 <byronajcet@gmail.com>
2026-03-09 09:00:04 -0600

    docs: :memo: Add creacion de menu
    menu principal
    
    By Byron with Sergio

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
-print("Hola mundo")
+menu = """
+ ===========================================    
+         Administrador de Colección
+ ===========================================
+1. Añadir un Producto
+2. Eliminar productos
+3. Ver producots
+4. Salir
+ ===========================================
+ """   
+while True:
+ print(menu)
+ opc = input ("Selecciona una opción: ")
+ if opc == "1":
+    if opc == "2":
+     if opc == "3":
+      if opc == "4":
+       print("Feliz día")
+ break
+
+
